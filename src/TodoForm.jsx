1790929706fdd277132349f94725961de0c9bd2b
Diego Krefta <diego.krefta@gmail.com>
2018-11-29 09:43:48 -0200

    add useState

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 
+const TodoForm = ({ saveTodo }) => {
+  const [value, setValue] = useState("");
 
-const TodoForm = ({saveTodo}) => {
- 
   return (
-    <form>
-    <TextField
-    variant='outlined'
-    placeholder='Add Todo'
-    margin='normal'
-    />
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+
+        saveTodo(value);
+      }}
+    >
+      <TextField
+        variant="outlined"
+        placeholder="Add Todo"
+        margin="normal"
+        onChange={event => {
+          setValue(event.target.value);
+        }}
+        value={value}
+      />
     </form>
-  )
-}
+  );
+};
 
 export default TodoForm;
-
